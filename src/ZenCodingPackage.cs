e724598c3a8fa77cf13aa5b11a786fbc09918a96
Mads Kristensen <post@madskristensen.net>
2017-01-20 10:04:45 -0800

    Updated package and assembly info

--- src/ZenCodingPackage.cs
+++ src/ZenCodingPackage.cs
@@ -5,15 +5,9 @@ using System.Runtime.InteropServices;
 namespace ZenCodingVS
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    [Guid(ZenCodingPackage.PackageGuidString)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
+    [Guid("7c1ab83e-2477-4554-a538-f3204dcd63dd")]
     public sealed class ZenCodingPackage : Package
     {
-        public const string PackageGuidString = "7c1ab83e-2477-4554-a538-f3204dcd63dd";
-
-        protected override void Initialize()
-        {
-            base.Initialize();
-        }
     }
 }
